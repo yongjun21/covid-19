be5cab730e4e3bbdbb1160e2d75cd0f76a8b8377
Yong Jun <yjthong.2009@smu.edu.sg>
2020-02-05 19:31:57 +0800

    Simplify feature matching

--- src/components/InteractiveMap.vue
+++ src/components/InteractiveMap.vue
@@ -32,36 +32,7 @@ const CHINA = {
 const MUNICIPALITIES = ['北京市', '天津市', '上海市', '重庆市']
 
 const CORRECTIONS = [
-  { provinceName: '湖北省', givenName: '恩施州', correctName: '恩施土家族苗族自治州' },
-  { provinceName: '河南省', givenName: '长垣县', correctName: '长垣市' },
-  { provinceName: '湖南省', givenName: '湘西自治州', correctName: '湘西土家族苗族自治州' },
-  { provinceName: '重庆市', givenName: '石柱县', correctName: '石柱土家族自治县' },
-  { provinceName: '重庆市', givenName: '秀山县', correctName: '秀山土家族苗族自治县' },
-  { provinceName: '四川省', givenName: '甘孜州', correctName: '甘孜藏族自治州' },
-  { provinceName: '四川省', givenName: '凉山州', correctName: '凉山彝族自治州' },
-  { provinceName: '四川省', givenName: '阿坝州', correctName: '阿坝藏族羌族自治州' },
-  { provinceName: '四川省', givenName: '凉山', correctName: '凉山彝族自治州' },
-  { provinceName: '上海市', givenName: '浦东', correctName: '浦东新区' },
-  { provinceName: '云南省', givenName: '西双版纳', correctName: '西双版纳傣族自治州' },
-  { provinceName: '云南省', givenName: '德宏', correctName: '德宏傣族景颇族自治州' },
-  { provinceName: '海南省', givenName: '陵水县', correctName: '陵水黎族自治县' },
-  { provinceName: '海南省', givenName: '琼中县', correctName: '琼中黎族苗族自治县' },
-  { provinceName: '海南省', givenName: '陵水', correctName: '陵水黎族自治县' },
-  { provinceName: '海南省', givenName: '琼中', correctName: '琼中黎族苗族自治县' },
-  { provinceName: '海南省', givenName: '昌江', correctName: '昌江黎族自治县' },
-  { provinceName: '海南省', givenName: '乐东', correctName: '乐东黎族自治县' },
-  { provinceName: '黑龙江省', givenName: '大兴安岭', correctName: '大兴安岭地区' },
-  { provinceName: '吉林省', givenName: '延边', correctName: '延边朝鲜族自治州' },
-  { provinceName: '新疆维吾尔自治区', givenName: '伊犁州', correctName: '伊犁哈萨克自治州' },
-  { provinceName: '新疆维吾尔自治区', givenName: '第七师', correctName: '第七师（胡杨河市）' },
-  { provinceName: '新疆维吾尔自治区', givenName: '第八师', correctName: '石河子市' },
-  { provinceName: '新疆维吾尔自治区', givenName: '第九师', correctName: '额敏县' },
-  { provinceName: '贵州省', givenName: '黔南州', correctName: '黔南布依族苗族自治州' },
-  { provinceName: '贵州省', givenName: '黔西南州', correctName: '黔西南布依族苗族自治州' },
-  { provinceName: '贵州省', givenName: '黔东南州', correctName: '黔东南苗族侗族自治州' },
-  { provinceName: '天津市', givenName: '宁河区', correctName: '宁河县' },
-  { provinceName: '甘肃省', givenName: '甘南', correctName: '甘南藏族自治州' },
-  { provinceName: '内蒙古自治区', givenName: '赤峰', correctName: ['松山区', '红山区', '元宝山区', '林西县', '宁城县', '克什克腾旗', '巴林左旗', '巴林右旗', '阿鲁科尔沁旗', '翁牛特旗', '喀喇沁旗'] }
+  { provinceName: '黑龙江省', givenName: '大兴安岭', correctName: '大兴安岭地区' }
 ]
 
 let colorScale = chroma.scale('OrRd').gamma(GAMMA.color)
@@ -133,40 +104,25 @@ export default {
         colorScale.domain([0, max])
 
         toPlot.forEach(row => {
-          const matched = Array.isArray(row.matched) ? row.matched : [row.matched]
-          matched.forEach(name => {
-            const lvl = name.level
-            const state = {
-              name: row.cityName || row.provinceName,
-              confirmed: row.confirmedCount,
-              dead: row.deadCount,
-              color: colorScale(row.confirmedCount).hex()
-            }
-            map.setFeatureState({
-              source: 'china',
-              sourceLayer: 'china_boundaries_l' + lvl,
-              id: name.id
-            }, state)
-            map.setFeatureState({
-              source: 'china',
-              sourceLayer: 'china_places_l' + lvl,
-              id: name.id
-            }, state)
-          })
+          const lvl = row.matched.level
+          const state = {
+            name: row.cityName || row.provinceName,
+            confirmed: row.confirmedCount,
+            dead: row.deadCount,
+            color: colorScale(row.confirmedCount).hex()
+          }
+          map.setFeatureState({
+            source: 'china',
+            sourceLayer: 'china_boundaries_l' + lvl,
+            id: row.matched.id
+          }, state)
+          map.setFeatureState({
+            source: 'china',
+            sourceLayer: 'china_places_l' + lvl,
+            id: row.matched.id
+          }, state)
         })
       })
-
-      // map.addLayer({
-      //   id: 'l2_line',
-      //   source: 'china',
-      //   'source-layer': 'china_boundaries_l2',
-      //   type: 'line',
-      //   paint: {
-      //     'line-color': 'darkgrey',
-      //     'line-width': 1.2,
-      //     'line-opacity': 0.8
-      //   }
-      // })
 
       map.addLayer({
         id: 'l1_data',
@@ -198,6 +154,20 @@ export default {
         }
       })
 
+      /*
+      map.addLayer({
+        id: 'l2_line',
+        source: 'china',
+        'source-layer': 'china_boundaries_l2',
+        type: 'line',
+        paint: {
+          'line-color': 'darkgrey',
+          'line-width': 1.2,
+          'line-opacity': 0.8
+        }
+      })
+      */
+
       map.addLayer({
         id: 'l1_label',
         source: 'china',
@@ -214,12 +184,8 @@ export default {
       })
 
       map.on('mousemove', e => {
-        const featuresL1 = map.queryRenderedFeatures(e.point, {
-          layers: ['l1_data']
-        })
-        const featuresL2 = map.queryRenderedFeatures(e.point, {
-          layers: ['l2_data']
-        })
+        const featuresL1 = map.queryRenderedFeatures(e.point, { layers: ['l1_data'] })
+        const featuresL2 = map.queryRenderedFeatures(e.point, { layers: ['l2_data'] })
         if (featuresL2.length > 0 && featuresL2[0].state.confirmed > 0) {
           const data = [featuresL2[0].properties, featuresL2[0].state]
           tooltip.setData(data).trackPointer().addTo(map)
@@ -230,6 +196,7 @@ export default {
           tooltip.setData(null).remove()
         }
       })
+      map.on('movestart', e => tooltip.setData(null).remove())
     })
   }
 }
@@ -245,8 +212,8 @@ function getData () {
   ]).then(([names, provinces, cities]) => {
     const provinceNames = {}
     names.forEach(row => {
-      if (row.level_1_parent) {
-        provinceNames[row.level_1_parent].children.push(row)
+      if (row.level_1) {
+        provinceNames[row.level_1].children.push(row)
       } else {
         row.children = []
         provinceNames[row.id] = row
@@ -268,6 +235,10 @@ function getData () {
       if (!matched && !isCity) matched = children.find(c => c.name_chi === row.cityName + '市')
       if (!matched && !isCity) matched = children.find(c => c.name_chi === row.cityName + '县')
       if (!matched) {
+        const test = row.cityName.replace(/(自治)?(县|州)$/, '')
+        matched = children.find(c => c.name_chi.includes('自治') && c.name_chi.startsWith(test))
+      }
+      if (!matched) {
         const correction = CORRECTIONS.find(c => c.provinceName === row.provinceName && c.givenName === row.cityName)
         if (correction) {
           if (Array.isArray(correction.correctName)) {
@@ -285,9 +256,11 @@ function getData () {
     })
     const totalCases = provinces.reduce((sum, row) => sum + row.confirmedCount, 0)
     const lastUpdated = provinces.reduce((max, row) => row.modifyTime > max ? row.modifyTime : max, '')
-    // cities.filter(row => !row.matched).forEach(row => {
-    //   console.log(row.provinceName, row.cityName)
-    // })
+    cities.forEach(row => {
+      if (row.matched) return
+      if (MUNICIPALITIES.includes(row.provinceName)) return
+      console.log(row.provinceName, row.cityName)
+    })
     return [provinces, cities, totalCases, lastUpdated]
   })
 }
@@ -342,6 +315,10 @@ function createPopup (Content, options) {
     bottom: 0;
     z-index: 1;
     padding: 4px 8px;
+  }
+
+  .mapboxgl-popup {
+    pointer-events: none;
   }
 }
 </style>
